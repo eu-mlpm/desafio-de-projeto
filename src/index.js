2e93d84370de902b32d5633060505ac55bc8ee66
eu-mlpm <mlpm@outlook.com.br>
2023-12-22 16:17:28 -0300

    atualização do código para evitar futuros erros de encaixe no else if

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // Insira os dados do personagem: Nome e experiência.
-let dadosDoPersonagem = ["Shann", 0]
+let dadosDoPersonagem = ["Shann", 10001]
 let rankDoPersonagem = ""
 
 if(dadosDoPersonagem[1] <= 1000 ){
     rankDoPersonagem = "Ferro"
-} else if(dadosDoPersonagem[1] <= 2000){
+} else if(dadosDoPersonagem[1] >= 1001 && dadosDoPersonagem[1] <= 2000){
     rankDoPersonagem = "Bronze"
-} else if(dadosDoPersonagem[1] <= 5000){
+} else if(dadosDoPersonagem[1] >= 2001 && dadosDoPersonagem[1] <= 5000){
     rankDoPersonagem = "Prata"
-} else if(dadosDoPersonagem[1] <= 7000){
+} else if(dadosDoPersonagem[1] >= 5001 &&dadosDoPersonagem[1] <= 7000){
     rankDoPersonagem = "Ouro"
-} else if(dadosDoPersonagem[1] <= 8000){
+} else if(dadosDoPersonagem[1] >= 7001 &&dadosDoPersonagem[1] <= 8000){
     rankDoPersonagem = "Ascendente"
-} else if(dadosDoPersonagem[1] <= 10000){
+} else if(dadosDoPersonagem[1] >= 8001 &&dadosDoPersonagem[1] <= 10000){
     rankDoPersonagem = "Imortal"
 } else {
     rankDoPersonagem = "Radiante"
